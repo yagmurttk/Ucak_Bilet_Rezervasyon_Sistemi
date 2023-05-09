e449d1a3120363b66ecf431c3e852865abb066dd
Yağmur K <yagmurrkeserr1@gmail.com>
2023-05-09 07:51:50 +0300

    Add project files.

---
+++ Ucak_Bilet_Rezervasyon_Sistemi/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucak_Bilet_Rezervasyon_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text+" Tarih: "+dateTimePicker1.Text);
+        }
+    }
+}
